de2a911a92e95d61d0837e01c53055dcc0d90993
Kurt Niemi <kurtn718@gmail.com>
2024-12-21 19:32:31 -0500

    Stub for Saved Thoughts

---
+++ app/(dashboard)/savedthoughts/page.js
@@ -0,0 +1,8 @@
+export default function SavedThoughtsPage() {
+  return (
+    <div className='min-h-screen flex justify-center items-center'>
+      <h1>Saved Thoughts</h1>
+      This feature is coming soon!
+    </div>
+  );
+}
